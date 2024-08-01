4bbcfd1db78d5c64ba655c1c2d185dc671fa8b52
Harry Peach <harrypeach1999@gmail.com>
2024-08-01 20:35:21 +0000

    chore: update sveltekit + svelte version

--- wheel_frontend/src/routes/+page.svelte
+++ wheel_frontend/src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
-	import { Canvas, Layer, t, type Render } from 'svelte-canvas';
 	import { page } from '$app/stores';
+	import { PUBLIC_TMDB_API_KEY } from '$env/static/public';
 
 	const items = $page.url.searchParams.get('items');
-
-	let render: Render;
-	let x: any;
 </script>
 
 <html lang="en">
 	<h1>Yardley Movie Wheel</h1>
 	<h2>By Harry and Eldar</h2>
-
 </html>
 
 <style>
